7c2480a3ab526a5576ea4f6864095dadea8e363f
etoosammy01 <etoosammy01@gmail.com>
2026-07-23 13:39:53 +0100

    my second commit

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	request := Request{
+		Prompt: "What is recursion?",
+	}
+
+	data, err := json.Marshal(request)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println(string(data))
+}
